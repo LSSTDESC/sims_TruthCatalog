05682574e5ed0817dfe7f53c217a563ed017b451
James Chiang <jchiang87@gmail.com>
2020-01-24 13:08:25 -0700

    add is_variable and is_pointsource keyword arguments so that SNe can use the write_sqlite function

--- python/desc/sims_truthcatalog/galaxy_truth.py
+++ python/desc/sims_truthcatalog/galaxy_truth.py
@@ -52,7 +52,8 @@ def _good_indices(galaxies, bad_gs):
     return good_ixes
 
 def _write_sqlite(dbfile, galaxy_ids, ra, dec, redshift, flux_by_band_MW,
-                  flux_by_band_noMW, good_ixes):
+                  flux_by_band_noMW, good_ixes, is_variable=0,
+                  is_pointsource=0):
     with sqlite3.connect(dbfile) as conn:
         conversion_utils.write_column_descriptions(conn)
         cursor = conn.cursor()
@@ -69,7 +70,7 @@ def _write_sqlite(dbfile, galaxy_ids, ra, dec, redshift, flux_by_band_MW,
         #print("Created table if not exists truth_summary")
         values = ((int(galaxy_ids[i_obj]),int(-1),
                    ra[i_obj],dec[i_obj],
-                   redshift[i_obj], 0, 0,
+                   redshift[i_obj], is_variable, is_pointsource,
                    flux_by_band_MW['u'][i_obj], flux_by_band_MW['g'][i_obj],
                    flux_by_band_MW['r'][i_obj], flux_by_band_MW['i'][i_obj],
                    flux_by_band_MW['z'][i_obj], flux_by_band_MW['y'][i_obj],
