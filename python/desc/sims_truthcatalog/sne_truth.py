7a78c4bcbe75aa60a897146a03d3477917d4395d
James Chiang <jchiang87@gmail.com>
2020-01-31 13:13:37 -0800

    add method to write SN Auxiliary Truth table

--- python/desc/sims_truthcatalog/sne_truth.py
+++ python/desc/sims_truthcatalog/sne_truth.py
@@ -42,3 +42,30 @@ class SNeTruthWriter:
                      flux_by_band_MW={_: zeros for _ in 'ugrizy'},
                      flux_by_band_noMW={_: zeros for _ in 'ugrizy'},
                      good_ixes=range(len(self.sne_df)))
+
+    def write_auxiliary_truth(self):
+        """
+        Write the SN Auxiliary truth table from the SNe db file.
+        This is almost a direct transcription of the columns.
+        """
+        table_name = 'sn_auxiliary_info'
+        cmd = f'''CREATE TABLE IF NOT EXISTS {table_name}
+        (id TEXT, host_galaxy_id BIGINT, ra DOUBLE, dec DOUBLE,
+        t0 FLOAT, x0 FLOAT, x1 FLOAT, c FLOAT, redshift FLOAT)'''
+        with sqlite3.connect(self.outfile) as conn:
+            cursor = conn.cursor()
+            cursor.execute(cmd)
+            conn.commit()
+            values = ((str(self.sne_df['snid_in'][i_obj]),
+                       int(self.sne_df['galaxy_id'][i_obj]),
+                       self.sne_df['snra_in'][i_obj],
+                       self.sne_df['sndec_in'][i_obj],
+                       self.sne_df['t0_in'][i_obj],
+                       self.sne_df['x0_in'][i_obj],
+                       self.sne_df['x1_in'][i_obj],
+                       self.sne_df['c_in'][i_obj],
+                       self.sne_df['z_in'][i_obj])
+                      for i_obj in range(len(self.sne_df)))
+            cursor.executemany(f'''INSERT INTO {table_name}
+                                   VALUES (?,?,?,?,?,?,?,?,?)''', values)
+            conn.commit()
