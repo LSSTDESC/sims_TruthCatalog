b6f9f0f9ca5247d65f0243ae2e0d5f359618d896
James Chiang <jchiang87@gmail.com>
2020-02-15 18:12:30 -0800

    add test_variability_truth function

--- tests/test_agn_truth.py
+++ tests/test_agn_truth.py
@@ -14,14 +14,16 @@ class AGNTruthWriterTestCase(unittest.TestCase):
     '''
     def setUp(self):
         self.outfile = 'agn_truth_cat.db'
-        self.agn_db_file = os.path.join(os.environ['SIMS_TRUTHCATALOG_DIR'],
-                                        'data', 'agn_cosmoDC2_v1.1.4_small.db')
+        self.data_dir \
+            = os.path.join(os.environ['SIMS_TRUTHCATALOG_DIR'], 'data')
+        self.agn_db_file \
+            = os.path.join(self.data_dir, 'agn_cosmoDC2_v1.1.4_small.db')
 
     def tearDown(self):
         if os.path.isfile(self.outfile):
             os.remove(self.outfile)
 
-    def test_write(self):
+    def test_truth_summary(self):
         '''
         Test the AGNTruthWriter.write method.
         '''
@@ -42,7 +44,7 @@ class AGNTruthWriterTestCase(unittest.TestCase):
         self.assertEqual(row['host_galaxy'], 1250442285)
         self.assertAlmostEqual(row['redshift'], 0.517463326454163)
 
-    def test_write_auxiliary(self):
+    def test_auxiliary_truth(self):
         '''
         Test the AGNTruthWriter.write_auxiliary_truth method.
         '''
@@ -64,6 +66,16 @@ class AGNTruthWriterTestCase(unittest.TestCase):
         self.assertEqual(row['host_galaxy'], galaxy_id)
         self.assertAlmostEqual(row['M_i'], -22.5594939688854)
         self.assertEqual(row['seed'], 7649944)
+
+    def test_variability_truth(self):
+        """
+        Test some light curve values for AGNs from the test AGN catalog.
+        """
+        opsim_db_file = os.path.join(self.data_dir,
+                                     'minion_1016_desc_dithered_v4_small.db')
+        agn_truth_writer = AGNTruthWriter(self.outfile, self.agn_db_file)
+        agn_truth_writer.write_variability_truth(opsim_db_file, max_rows=100,
+                                                 verbose=True)
 
 
 if __name__ == '__main__':
